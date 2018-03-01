16aaabc764bdd1b94566f7642725af782c6bf09a
raemcphail <raemcphail@gmail.com>
2018-02-28 19:04:35 -0700

    Started using ANT

--- firmware_mpgl1/application/mpgl1_board_test.h
+++ firmware_mpgl1/application/mpgl1_board_test.h
@@ -24,9 +24,9 @@ Constants / Definitions
 
 /* BoardTest ANT radio settings */
 /* MAKE SURE TO CHANGE THE u8 and u32 values for DEVICEID */
-#define	ANT_DEVICEID_LO_BOARDTEST			        (u8)0x34
-#define ANT_DEVICEID_HI_BOARDTEST		          (u8)0x12
-#define ANT_DEVICEID_DEC_BOARDTEST	          (u32)4660
+#define	ANT_DEVICEID_LO_BOARDTEST			        (u8)0x89
+#define ANT_DEVICEID_HI_BOARDTEST		          (u8)0x04
+#define ANT_DEVICEID_DEC_BOARDTEST	          (u32)1161
 
 /* IF YOU CHANGE DEVICE TYPE OR TRANSMISSION TYPE, YOU MUST CHANGE 
 THE STARTUP TEXT MESSAGE IN BoardTestSM_SetupAnt */
